7fbca77a69887b503b171a9ec7ef9a6c921f076a
Lisa <lisa.roxane@gmail.com>
2022-02-23 11:59:39 -0330

    update scripts.js
    for loop to display items in pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,13 @@ let pokemonList = [
         types: ['poison', 'ground']
     }
 ];
+
+//for loop to display the name and height of each pokemon in the array to the DOM
+//add remark to highlight big pokemon
+for (let i=0; i < repository.length; i++) {
+    if (repository[i].height < 9) {
+        document.write("<p>" + repository[i].name + " (height: " + repository[i].height + ") "  + "</p>")
+    } else {
+      document.write("<p>" + repository[i].name + " (height: " + repository[i].height + ") " + "-Wow, that's a big Pokemon!" + "</p>")
+    }
+} 
