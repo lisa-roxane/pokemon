492cac752d7f814fa4d55a008cd26c1646d5c1d7
Lisa <lisa.roxane@gmail.com>
2022-02-23 11:34:18 -0330

    Initial Commit
    Create:
    index.html
    scripts.js
    styles.css
    README.md

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 6,
+        types: ['fire']
+    },
+    {
+        name: 'Butterfree',
+        height: 11,
+        types: ['bug', 'flying']
+    },
+    {
+        name: 'Nidoqueen',
+        height: 13,
+        types: ['poison', 'ground']
+    }
+];
